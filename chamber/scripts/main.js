bd0bcbf30b0faa747bad41a9ce88941ed90ee92d
Igberry <davidigberichuks@gmail.com>
2024-09-28 23:51:57 +0100

    chamber

---
+++ chamber/scripts/main.js
@@ -0,0 +1,8 @@
+// scripts/main.js
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+document.getElementById("last-modified").textContent = document.lastModified;
